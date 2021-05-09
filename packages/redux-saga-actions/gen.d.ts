037bdcae5801d14b7083464a245af21dcc794135
zsirfs <zhenglfsir@gmail.com>
2021-05-09 23:32:22 +0800

    fix: 修复类型错误

--- packages/redux-saga-actions/gen.d.ts
+++ packages/redux-saga-actions/gen.d.ts
@@ -1,9 +1,14 @@
 import { Store } from 'redux';
-import { generate, IModel } from './es';
+import { IModel } from './es';
 
-type GenerateReturn = ReturnType<typeof generate>;
-export interface IWrapperExport<T> extends GenerateReturn<T> {
+export interface IWrapperExport<T> {
   store: Store;
+  state: { [k in keyof T]: T[k]['state'] };
+  useReduxState: <T_1 extends unknown = { [k in keyof T]: T[k]['state'] }>(
+    cb?: ((state: { [k in keyof T]: T[k]['state'] }) => T_1) | undefined
+  ) => T_1 extends { [k in keyof T]: T[k]['state'] }
+    ? { [k in keyof T]: T[k]['state'] }
+    : T_1;
 }
 export function wrapper<
   T extends Record<string, IModel> = Record<string, IModel>
